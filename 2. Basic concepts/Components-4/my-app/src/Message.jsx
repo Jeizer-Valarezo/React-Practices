c9ca6282d272ebd9df54c28d44a42b66d37d71bc
Jeizer-Valarezo <nakellava@hotmail.com>
2024-02-07 11:20:58 +0100

    Component 4

---
+++ 2. Basic concepts/Components-4/my-app/src/Message.jsx
@@ -0,0 +1,4 @@
+export const Message = () => {
+  return <p>What a beautiful day!</p>
+  
+}
---
+++ 2. Basic concepts/Components-4/my-app/src/app.jsx
@@ -0,0 +1,9 @@
+import { HelloWorld } from './HelloWorld'
+import { createRoot } from 'react-dom/client'
+
+const helloWorldElement = <HelloWorld/>
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(helloWorldElement)
