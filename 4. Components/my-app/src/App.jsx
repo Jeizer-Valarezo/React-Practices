117b7c0f665c49b53c850143be27271692aa8708
Jeizer-Valarezo <nakellava@hotmail.com>
2024-02-22 12:24:55 +0100

    Route 1

--- 4. Components/my-app/src/App.jsx
+++ 4. Components/my-app/src/App.jsx
@@ -1,44 +1,11 @@
-import React, { useState } from "react";
-import { Container } from "./Components/Container";
-import { InteractiveWelcome } from "./Components/InteractiveWelcome";
-import { LanguageContext } from "./Components/LanguageContext";
-import { Clock } from "./Components/Clock";
-import { GithubUser } from "./Components/GithubUser";
-import { GithubUsers } from "./Components/GithubUsers";
-import { Counter } from "./Components/Counter";
-import { Form } from "./Components/Form";
-import { CurrentLocation } from "./Components/CurrentLocation";
+import { Route, Routes } from "react-router-dom";
+import { Welcome } from "./Components/Welcome"
 
 export function App() {
-  const [language, setLanguage] = useState('en');
-
-  const handleLanguageChange = (event) => {
-    setLanguage(event.target.value);
-  };
-
-  const onLogin = (loginData) => {
-    console.log("Login data:", loginData);
-  };
-
+  
   return (
-    <LanguageContext.Provider value={language}>
-      <Container title={<h1>My awesome Application</h1>}>
-        
-        <InteractiveWelcome />
-        <div>
-          <label>Select Language: </label>
-          <select value={language} onChange={handleLanguageChange}>
-            <option value="en">English</option>
-            <option value="es">Español</option>
-          </select>
-        </div>
-        <Clock />
-        <GithubUser />
-        
-        <Counter initialCounter={0} incrementAmount={1} decrementAmount={1} resetAmount={0}/>
-        <Form onLogin={onLogin}/>
-        <CurrentLocation/>
-      </Container>
-    </LanguageContext.Provider>
+    <Routes>
+      <Route path="/" element={<Welcome name="ur sister bf"/>}/>
+    </Routes>
   );
 }
