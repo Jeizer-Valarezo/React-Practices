bf48272f1dac72d85bb676865361fe6d10474b7d
Jeizer-Valarezo <nakellava@hotmail.com>
2024-02-22 12:29:09 +0100

    Route counter

--- 4. Components/my-app/src/App.jsx
+++ 4. Components/my-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Welcome } from "./Components/Welcome"
+import { Counter } from "./Components/Counter"
 
 export function App() {
   
   return (
     <Routes>
       <Route path="/" element={<Welcome name="ur sister bf"/>}/>
+      <Route path="counter" element={<Counter initialCounter={0} incrementAmount={1} decrementAmount={1} resetAmount={0}/>}/>
     </Routes>
   );
 }
